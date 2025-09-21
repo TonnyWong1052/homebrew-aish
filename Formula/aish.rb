67fc13371e2b81d01900698407273e29d84ed253
TonnyWong <101496194+TonnyWong1052@users.noreply.github.com>
2025-09-21 20:59:27 +0800

    aish 0.0.1: retag initial release; update sha256 and test

--- Formula/aish.rb
+++ Formula/aish.rb
@@ -8,9 +8,9 @@
 class Aish < Formula
   desc "AI shell helper"
   homepage "https://github.com/TonnyWong1052/aish"
-  url "https://github.com/TonnyWong1052/aish/archive/refs/tags/v0.0.5.tar.gz"
-  sha256 "17fcda9f7dd6f5b58ba138fd401a2e7d2b3c3d0345ef5659c8b646fb1a247480"
-  version "0.0.5"
+  url "https://github.com/TonnyWong1052/aish/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "1fad17600f1b11d6ffd07dafe73f37dec09586f00b8d3370472c73ab9262b9d4"
+  version "0.0.1"
   license "MIT"
 
   depends_on "go" => :build
@@ -31,6 +31,6 @@ class Aish < Formula
   test do
     # This test is basic and just checks if the binary can be executed.
     # You might want to add a more comprehensive test here.
-    assert_match "aish v0.0.5", shell_output("#{bin}/aish --version")
+    assert_match "aish v0.0.1", shell_output("#{bin}/aish --version")
   end
 end
